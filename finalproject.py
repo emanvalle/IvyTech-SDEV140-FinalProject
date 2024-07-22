5fb339168e09fb055b72e56a745ccdbcd6eccb52
Eric Valle <emanvalle@gmail.com>
2024-07-22 07:46:43 -0700

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -7,23 +7,25 @@ Short Desc:
 
 """
 #Importing necessary libraries for GUI
+from tkinter import *
 import tkinter as tk
 
 def openNewWindow():
 	
-	# Toplevel object which will 
-	# be treated as a new window
-	newWindow = Toplevel(root)
+	# Toplevel object decalred as newWindow
+	new_window = Toplevel(root)
   
 	# sets the title of the
 	# Toplevel widget
-	newWindow.title("New Window")
+	new_window.title("New Window")
 
 	# sets the geometry of toplevel
-	newWindow.geometry("200x200")
+	new_window.geometry("200x200")
 
 	# A Label widget to show in toplevel
-	newWindow.Label(newWindow, text ="This is a new window").pack()
+	new_window_label = tk.Label(new_window, text="Upcoming Events", font=('Helvetica', 20))
+	new_window_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+	#new_window.Label(new_window, text ="This is a new window").pack()
 
 #Create the main window with title Guessing Game
 root = tk.Tk()
@@ -33,16 +35,32 @@ root.title("Marty's Music")
 title_label = tk.Label(root, text="Marty's Music", font=('Helvetica', 20))
 title_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-#Button to open new window
-btn = Button(root, text ="Click to open a new window", 
-             command = openNewWindow)
+#GUI Description Label
+GUI_description = """Welcome to Marty's Music GUI! This GUI provides 
 
+"""
+title_label = tk.Label(root, text="Marty's Music", font=('Helvetica', 12))
+title_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+
+#Button to open new window
+btn = Button(root, text ="Click to open a new window", command = openNewWindow)
+btn.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
 
 root.mainloop()
-top.mainloop()
 
 """
+#Title Image
+#Open Title Image file
+title_image = open("title_image.HEIC", "r")
+title_image_label = tk.Label(root, image=title_image)
+title_image_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+
+
+
+
+
 # This will import all the widgets
 # and modules which are available in
 # tkinter and ttk module
